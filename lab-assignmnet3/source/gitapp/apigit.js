62501d8e36b31e1b83700adeca197cd8922297a3
naveenanallamotu <naveena.nallamothu@gmail.com>
2017-06-30 14:03:20 -0500

    bugfix

--- lab-assignmnet3/source/gitapp/apigit.js
+++ lab-assignmnet3/source/gitapp/apigit.js
@@ -1,16 +1,32 @@
 /**
  * Created by NAVEENA on 27-06-2017.
  */
-$(document).ready(function(){
-    $('#searchUser').on('keyup',function(e){
-        var username = e.target.value;
-        $.ajax({
-            url:'https://api.github.com/users/'+username,
-        }).done(function(user){
-            $('#profile').html(
-                $(user.name)
-            );
-        });
+function myfunction(){
+    var str1=document.getElementById("searchUser").value;
+    var url = "https://api.github.com/users/"+str1;
+    $("#data").empty();
+    $.ajax({
+        'url': url,
+        'type': 'GET',
+        'success': function (data) {
+            follow = data.followers;
+            if(follow >= 5) {
+                var table = '';
+                table += "<label id='tableLabel'></label>";
+                table += "<tbody>";
+                table += "<tr><td>Name:</td><td>" + data.login + "</td></tr>";
+                table += "<tr><td>ID:</td><td>" + data.id + "</td></tr>";
+                table += "<tr><td>Profile picture</td><td><img src='" + data.avatar_url + "'</td></tr>";
+                table += "<tr><td>User's Link:</td><td>" + data.html_url + "</td></tr>";
+                table += "<tr><td>Date of Creation of  account</td><td>" + data.created_at + "</td></tr>";
+                table += "<tr><td>Followers:</td><td>" + data.followers + "</td></tr>";
+                table += "<tr><td>PublicRepositories count:</td><td>" + data.public_repos + "</td></tr>";
+                table += "</tbody>";
+                document.getElementById("data").innerHTML = table;
+            }
+            else{
+                window.alert(data.login +" "+ "have Followers less than 5.")
+            }
+        }
     });
-
-});
+}
